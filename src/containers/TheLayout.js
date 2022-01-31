ecc94ad0f377f021747260749cfeb196f5e0f5b1
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-01-31 02:13:30 -0300

    Login Verification

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TheContent, TheSidebar, TheFooter } from './index'
+import useApi from '../services/api'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 const TheLayout = () => {
 
+  const api = useApi();
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      if (api.getToken()) {
+        const result = await api.validateToken();
+        if (result.error === '') {
+          setLoading(false);
+        } else {
+          alert(result.error);
+          history.push('/login');
+        }
+      } else {
+        history.push('/login');
+
+      }
+    }
+    checkLogin();
+
+  }, []);
+
   return (
     <div className="c-app c-default-layout">
-      <TheSidebar />
-      <div className="c-wrapper">
-        <div className="c-body">
-          <TheContent />
-        </div>
-        <TheFooter />
-      </div>
+      {!loading &&
+        <>
+          <TheSidebar />
+          <div className="c-wrapper">
+            <div className="c-body">
+              <TheContent />
+            </div>
+            <TheFooter />
+          </div>
+        </>
+      }
     </div>
   )
 }
