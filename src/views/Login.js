5eb71d354367c27c81f507a6691d3a63b4aa29c4
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-02-01 02:14:45 -0300

    Login Pag - Part 1

---
+++ src/views/Login.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CInput,
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CRow
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import useApi from '../services/api'
+
+const Login = () => {
+  const api = useApi();
+  const history = useHistory();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const makeLoginButton = async () => {
+    if (email && password) {
+      const result = await api.login(email, password);
+      if (result.error === '') {
+        localStorage.setItem('token', result.token);
+        history.push('/');
+      }
+    } else {
+      alert("Digite os dados");
+    }
+  }
+
+  return (
+
+    <div className="c-app c-default-layout flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md="5">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p className="text-muted">Digite seus dados de acesso</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+
+                      <CInput type="text" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs="6">
+                        <CButton
+                          color="primary"
+                          className="px-4"
+                          onClick={makeLoginButton}
+                        >
+                          Entrar
+                        </CButton>
+                      </CCol>
+
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
