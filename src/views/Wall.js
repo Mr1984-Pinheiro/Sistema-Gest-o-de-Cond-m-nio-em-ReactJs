921bf84556ba4ae95d753c082d86eae587d1dcd8
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-02-04 23:48:32 -0300

    Avisos 1

---
+++ src/views/Wall.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import {
+
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import useApi from '../services/api'
+
+export default () => {
+    return (
+        
+    );
+}
